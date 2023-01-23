0cf0abf1c11371806de1ec09939c8f4ce02a8010
eemilhaa <eemil.haapanen@gmail.com>
2023-01-23 11:17:19 +0000

    3.6: error on non unique names

--- index.js
+++ index.js
@@ -66,6 +66,11 @@ app.post("/api/persons", (request, response) => {
       error: "name or number missing"
     })
   }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique"
+    })
+  }
   const person = {
     name: body.name,
     number: body.number,
