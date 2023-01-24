4ac8d6faa30776694c3f040ba3c26ffae833525d
eemilhaa <eemil.haapanen@gmail.com>
2023-01-24 12:31:08 +0000

    3.12: done

--- mongo.js
+++ mongo.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+const getArgs = () => {
+  if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+  }
+  return {
+    password: process.argv[2],
+    name: process.argv[3],
+    number: process.argv[4],
+  }
 }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
+const {password, name, number} = getArgs()
 
 const url =
   `mongodb+srv://fullstack:${password}@cluster0.ooro7n6.mongodb.net/fullstack-3?retryWrites=true&w=majority`
@@ -20,16 +25,37 @@ const personSchema = new mongoose.Schema({
   number: String,
   id: Number,
 })
-
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-  name: name,
-  number: number,
-  id: Math.floor(Math.random() * 10000000),
-})
+const addPerson = (name, number) => {
+  const person = new Person({
+    name: name,
+    number: number,
+    id: Math.floor(Math.random() * 10000000),
+  })
+  person
+    .save()
+    .then(result => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+}
 
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+const findAll = () => {
+  Person.find({})
+    .then(result => {
+      console.log("phonebook:")
+      result.forEach(person => {
+        console.log(
+          `${person.name} ${person.number}`
+        )
+      })
+      mongoose.connection.close()
+    })
+}
+
+if (!name || !number) {
+  findAll()
+} else {
+  addPerson(name, number)
+}
