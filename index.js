bec2128460ac8138d47f6ffe50405c914d8f34a1
eemilhaa <eemil.haapanen@gmail.com>
2023-01-25 15:03:19 +0000

    remove unecessary array

--- index.js
+++ index.js
@@ -12,29 +12,6 @@ app.use(express.json())
 app.use(logger)
 app.use(cors())
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-]
-
 app.get("/api/persons", (request, response) => {
   Person.find({})
     .then(persons => {
