006c5da397306d773586eec75e728d5c78469f09
eemilhaa <eemil.haapanen@gmail.com>
2023-01-25 15:19:26 +0000

    3.19: add validation

--- index.js
+++ index.js
@@ -52,11 +52,6 @@ app.delete("/api/persons/:id", (request, response, next) => {
 
 app.post("/api/persons", (request, response, next) => {
   const body = request.body
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number missing"
-    })
-  }
   const person = new Person ({
     name: body.name,
     number: body.number,
@@ -85,6 +80,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if ( error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
